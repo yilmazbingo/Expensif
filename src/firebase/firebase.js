3f17703556c29c9b61a73e75e4ef308d01876473
bingolusa <47233790+yilmazbingo@users.noreply.github.com>
2019-08-24 13:34:19 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,6 @@
 import * as firebase from "firebase";
 
-const config = {
-  apiKey: "AIzaSyDlBdjxk6bsxFOBEbH2KuWWS59k3EMeUUU",
-  authDomain: "budget-3db62.firebaseapp.com",
-  databaseURL: "https://budget-3db62.firebaseio.com",
-  projectId: "budget-3db62",
-  storageBucket: "budget-3db62.appspot.com",
-  messagingSenderId: "381593832065",
-  appId: "1:381593832065:web:fed424c3b9d1e8b4"
-};
+
 
 firebase.initializeApp(config);
 
